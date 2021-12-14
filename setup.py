ab378f8e0f6f642546271354477917c63742dfe6
kayagokalp <kayagokalp@sabanciuniv.edu>
2021-12-15 01:00:32 +0300

    setup.py update

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name='LESS',
+setup(name='LESS-MPI',
       version='0.1.0',
       description='Learning with Subset Stacking - MPI',
       url='git@github.com:sibirbil/LESS-MPI.git',
-      maintainer='Ilker Birbil',
-      maintainer_email='s.i.birbil@uva.nl',
+      maintainer='Kaya Gokalp',
+      maintainer_email='kayagokalp@sabanciuniv.edu',
       license='MIT',
-      packages=['less'],
+      packages=['less-learn-mpi'],
       zip_safe=False,
       install_requires=[
+        'mpi4py>=3.0.0'
         'scikit-learn>=1.0.1'
       ]),
