312de7c8fea2fa7afbba49cc93214c5183ce621d
kayagokalp <kayagokalp@sabanciuniv.edu>
2021-12-15 13:50:52 +0300

    setup.py finalized

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='less-learn-mpi',
-      version='0.1.0',
+      version='0.1.1',
       description='Learning with Subset Stacking - MPI',
       url='https://github.com/sibirbil/LESS-MPI',
       maintainer='Kaya Gokalp',
